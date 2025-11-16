3fddd70799ec160e9700be6cf1da4e6d283bdfc0
js2-llm <869063519@qq.com>
2025-11-16 20:18:09 +0000

    batch experiments

---
+++ experiments/control_variables/baseline-max_token.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+experiment_name="deepseek-baseline"
+model_name=("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
+dataset=("gsm8k_test")
+temperature=(0)
+max_tokens=(16384 32768)
+num_samples=(1)
+
+for model in ${model_name[@]}; do
+    for ds in ${dataset[@]}; do
+        for num_samples in ${num_samples[@]}; do
+            for temperature in ${temperature[@]}; do
+                for max_tokens in ${max_tokens[@]}; do
+                    python scripts/run_baseline.py \
+                        --model_name_or_path $model \
+                        --dataset $ds \
+                        --num_samples $num_samples \
+                        --temperature $temperature \
+                        --max_tokens $max_tokens \
+                        --save_dir results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens
+                done
+            done
+        done
+    done
+done
---
+++ experiments/control_variables/seal-max_token.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+experiment_name="deepseek-seal"
+model_name=("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
+dataset=("gsm8k_test")
+temperature=(0)
+max_tokens=(16384 32768)
+num_samples=(1)
+vector_scale=(1)
+
+for model in ${model_name[@]}; do
+    for ds in ${dataset[@]}; do
+        for num_samples in ${num_samples[@]}; do
+            for temperature in ${temperature[@]}; do
+                for max_tokens in ${max_tokens[@]}; do
+                    for scale in ${vector_scale[@]}; do
+                        python scripts/run_seal.py \
+                            --model_name_or_path $model \
+                            --dataset $ds \
+                            --temperature $temperature \
+                            --max_tokens $max_tokens \
+                            --num_samples $num_samples \
+                            --vector_dir ./vectors/seal/$model \
+                            --scale $scale \
+                            --save_dir results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens/$scale
+                    done
+                done
+            done
+        done
+    done
+done
