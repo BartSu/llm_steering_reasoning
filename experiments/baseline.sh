967c087c3c9b3971c8c95aeba0f3e43b9320d6e9
js2-llm <869063519@qq.com>
2025-11-19 03:19:46 +0000

    llm-as-a-judge

--- experiments/baseline.sh
+++ experiments/baseline.sh
@@ -4,7 +4,7 @@ model_name=("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
 dataset=("gsm8k_test")
 temperature=(0.7)
 max_tokens=(10000)
-num_samples=(1 256)
+num_samples=(1)
 
 for model in ${model_name[@]}; do
     for ds in ${dataset[@]}; do
@@ -12,21 +12,25 @@ for model in ${model_name[@]}; do
             for temperature in ${temperature[@]}; do
                 for max_tokens in ${max_tokens[@]}; do
                     # if the directory results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens exists, skip
-                    if [ -d "results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens/predictions.jsonl" ]; then
-                        continue
+                    if [ ! -f "results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens/predictions.jsonl" ]; then
+                        echo "Running baseline for $model $ds $num_samples $temperature $max_tokens"
+                        python scripts/run_baseline.py \
+                            --model_name_or_path $model \
+                            --dataset $ds \
+                            --num_samples $num_samples \
+                            --temperature $temperature \
+                            --max_tokens $max_tokens \
+                            --save_dir results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens
                     fi
-                    python scripts/run_baseline.py \
-                        --model_name_or_path $model \
-                        --dataset $ds \
-                        --num_samples $num_samples \
-                        --temperature $temperature \
-                        --max_tokens $max_tokens \
-                        --save_dir results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens
-
-                    # python scripts/run_judge.py \
-                    #     --model_name_or_path openai/gpt-oss-20b \
-                    #     --predictions_path results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens/predictions.jsonl \
-                    #     --save_dir results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens
+                    
+                    if [ ! -f "results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens/judgement.jsonl" ]; then
+                        echo "Running judgement for $model $ds $num_samples $temperature $max_tokens"
+                        python scripts/run_judge.py \
+                            --model_name_or_path openai/gpt-oss-120b \
+                            --predictions_path results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens/predictions.jsonl \
+                            --max_tokens 8192 \
+                            --save_dir results/$experiment_name/$model/$ds/$num_samples/$temperature/$max_tokens
+                    fi
                 done
             done
         done
